2e22b135bc8fe3110cf5165d88d7af6f1efa6a0e
crazyne55 <95332356+crazyne55@users.noreply.github.com>
2023-04-28 23:51:57 -0700

    almost there

---
+++ src/Planet.java
@@ -0,0 +1,38 @@
+public class Planet {
+    String name;
+    double radius;
+    double mass;
+    double distance;
+    double x;
+    double y;
+    double velX;
+    double velY;
+    public Planet(String name, double radius, double mass, double distance, double velX, double velY) {
+
+    }
+    public double getXPos() {
+        return x;
+    }
+    public double getYPos() {
+        return y;
+    }
+    public void moveTo(double newX, double newY) {
+        x = newX;
+        y = newY;
+    }
+    public double getXVel() {
+        return velX;
+    }
+    public double getYVel() {
+        return velY;
+    }
+    public void setXVel(double newVelX) {
+        velX = newVelX;
+    }
+    public void setYVel(double newVelY) {
+        velY = newVelY;
+    }
+    public String toString() {
+        return String.format("[%s] mass: %s; radius: %s; pos: (%s,%s); vel: (%s,%s)", name, mass, radius, x, y, velX, velY);
+    }
+}
---
+++ src/Sun.java
@@ -0,0 +1,29 @@
+public class Sun {
+    String name;
+    double radius;
+    double mass;
+    double temp;
+    double x;
+    double y;
+
+    public Sun(String name, double radius, double mass, double temp) {
+        this.name = name;
+        this.radius = radius;
+        this.mass = mass;
+        this.temp = temp;
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+    public double getXPos() { // renaming these from getXVel & getYVel to getXPos & getYPos since I am assuming this was a mix-up since SolarSystem.movePlanets() calls getXPos and getYPos
+        return x;
+    }
+    public double getYPos() {
+        return y;
+    }
+    public double getMass() {
+        return mass;
+    }
+    public String toString() {
+        return String.format("[%s] mass: %s; radius: %s; temp: %s; pos: (%s,%s)", name, mass, radius, temp, x, y);
+    }
+}
