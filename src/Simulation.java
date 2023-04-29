b8362f957921ed646c40c02eb643edf527fa064b
crazyne55 <95332356+crazyne55@users.noreply.github.com>
2023-04-28 23:57:07 -0700

    getting closer

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,12 @@
 public class Simulation {
     public static void main(String[] args) {
+        SolarSystem ss = new SolarSystem();
+        ss.addSun(new Sun("Sun", 1000000, 1600000, 134500000)); //random values
 
+        ss.addPlanet(new Planet("not earth", 100600000, 199999999, 12333333, 111, 222, 10, 155));
+
+        ss.movePlanets();
+        ss.showPlanets();
+        ss.movePlanets();
     }
 }
